6ee9ef370e00466faa4868996f4b6f3199f83621
eunicebes <eunicebes@gmail.com>
2019-03-18 10:46:22 +0800

    Breadth First Search in Graph

---
+++ BFS.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+class graph:
+    def __init__(self, num, is_undirected = True):
+        self.graph = defaultdict(set)
+        self.is_undirected = is_undirected
+        for i in range(num):
+            self.graph[i]
+
+    def addEdge(self, u, v):
+        self.graph[u].add(v)
+        if self.is_undirected:
+            self.graph[v].add(u)
+
+    def BFS(self, start):
+        visited = []
+        queue = []
+
+        queue.append(start)
+        visited.append(start)
+
+        while queue:
+            node = queue.pop(0)
+
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    visited.append(neighbor)
+                    queue.append(neighbor)
+
+        print(visited)
+
+G = graph(4)
+G.addEdge(0, 1)
+G.addEdge(0, 2)
+G.addEdge(1, 2)
+G.addEdge(2, 3)
+G.BFS(1)
